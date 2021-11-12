274ea1828ca059c4eeee523b8e35424002f75857
Tran Thanh Xuan <ttxuan0595@gmail.com>
2021-11-12 17:55:23 +0700

    Purge cache

--- getlink.js
+++ getlink.js
@@ -52,10 +52,8 @@ async function getCookieCsrf() {
   }
   loop = true;
   const browser = await puppeteer.launch({
-    'args' : [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
+    headless: true,
+    args: ['--no-sandbox','--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
   await page.goto('https://www.fshare.vn/site/login');
