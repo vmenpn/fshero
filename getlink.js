3cc07aaf1b312c26d7e7ffb298ae9990973e1b92
Tran Thanh Xuan <ttxuan0595@gmail.com>
2021-11-12 16:36:28 +0700

    b

--- getlink.js
+++ getlink.js
@@ -51,7 +51,10 @@ async function getCookieCsrf() {
     }, 1000 * 60 * 60);
   }
   loop = true;
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--use-gl=egl'],
+  });
   const page = await browser.newPage();
   await page.goto('https://www.fshare.vn/site/login');
   await page.type('#loginform-email', config.email);
